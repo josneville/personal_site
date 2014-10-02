ed7c7ce5e16f8579b82d1eccf280a7602f56e84f
Neville Jos <josneville@yahoo.com>
2014-10-02 16:57:53 -0500

    server.js port issue fixed I think

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var morgan = require('morgan');
 
 app.use(morgan('dev'));
 
-app.set("port", process.env.port || 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(app.get("port"), function(){
+app.listen(app.get('port'), function(){
 	console.log("Server running on port: 5000");
 });
