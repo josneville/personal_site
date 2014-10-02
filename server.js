5e975fe8721224014adccf45f594cb22ed649c56
Neville Jos <josneville@yahoo.com>
2014-10-02 16:53:47 -0500

    v1 done

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.set("port", process.env.port || 5000);
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(app.get("port"), function(){
+	console.log("Server running on port: 5000");
+});
